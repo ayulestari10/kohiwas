eeeb96f73bc240a7ad0de7c4dd25f31dbf24236e
ayulestari10 <lestariayu669@gmail.com>
2017-06-04 21:40:21 +0700

    revisi ke 2

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -100,6 +100,8 @@ class Admin extends MY_Controller{
 	{
 		$this->load->model('anggota_m');
 		$this->load->model('simpanan_m');
+		$this->load->model('jurnal_umum_m');
+		$this->load->model('buku_besar_m');
 		
 		if ($this->POST('simpan'))
 		{
@@ -116,9 +118,12 @@ class Admin extends MY_Controller{
 				redirect('admin/data_simpanan');
 				exit;	
 			}	
+			$this->simpanan_m->insert($this->data['simpanan']);
 
-			$this->load->model('jurnal_umum_m');
+			$this->data['getSimpanan'] = $this->simpanan_m->get_last_row();
+
 			$this->data['entri1'] = [
+				'id_relasi'		=> $this->data['getSimpanan']->id_simpanan,
 				'tgl'			=> $this->data['simpanan']['tgl_simpanan'],
 				'ket'			=> 'Kas Simpanan Wajib',
 				'debit'			=> $this->data['simpanan']['simpanan_wajib'],
@@ -126,17 +131,15 @@ class Admin extends MY_Controller{
 			];
 
 			$this->data['entri2'] = [
+				'id_relasi'		=> $this->data['getSimpanan']->id_simpanan,
 				'tgl'			=> $this->data['simpanan']['tgl_simpanan'],
 				'ket'			=> 'Kas Simpanan Sukarela',
 				'debit'			=> $this->data['simpanan']['simpanan_sukarela'],
 				'kredit'		=> $this->data['simpanan']['simpanan_sukarela'],
 			];
 
-			$this->simpanan_m->insert($this->data['simpanan']);
 			$this->jurnal_umum_m->insert($this->data['entri1']);
 			$this->jurnal_umum_m->insert($this->data['entri2']);
-			
-			$this->load->model('buku_besar_m');
 
 			$cek = $this->buku_besar_m->get_last_row();
 
@@ -151,6 +154,7 @@ class Admin extends MY_Controller{
 			}
 
 			$this->data['entri3'] = [
+				'id_relasi'		=> $this->data['getSimpanan']->id_simpanan,
 				'tgl'			=> $this->data['simpanan']['tgl_simpanan'],
 				'ket'			=> 'Simpanan Wajib',
 				'ref'			=> '103',
@@ -161,6 +165,7 @@ class Admin extends MY_Controller{
 			];
 			
 			$this->data['entri4'] = [
+				'id_relasi'		=> $this->data['getSimpanan']->id_simpanan,
 				'tgl'			=> $this->data['simpanan']['tgl_simpanan'],
 				'ket'			=> 'Simpanan Sukarela',
 				'ref'			=> '104',
@@ -187,7 +192,57 @@ class Admin extends MY_Controller{
 				'simpanan_sukarela'	=> $this->POST('edit_simpanan_sukarela')
 			];
 
+			// update jurnal umum
+			$this->data['entri1'] = [
+				'tgl'			=> $this->POST('edit_tgl_simpanan'),
+				'debit'			=> $this->POST('edit_simpanan_wajib'),
+				'kredit'		=> $this->POST('edit_simpanan_wajib')
+			];
+
+			$this->data['entri2'] = [
+				'tgl'			=> $this->POST('edit_tgl_simpanan'),
+				'debit'			=> $this->POST('edit_simpanan_sukarela'),
+				'kredit'		=> $this->POST('edit_simpanan_sukarela')
+			];
+
+			$cek = $this->buku_besar_m->get_previous_row('id_relasi', $this->POST('edit_id_simpanan'), 'Simpanan Wajib');
+
+			if(!isset($cek)){
+				$saldo_debit1 	= $this->POST('edit_simpanan_wajib');
+				$saldo_debit2	= $this->POST('edit_simpanan_sukarela');
+			}
+			if(isset($cek)){
+				$prev_saldo_debit 	= $cek->saldo_debit;
+				$saldo_debit1 		= $this->POST('edit_simpanan_wajib') + $prev_saldo_debit;
+				$saldo_debit2 		= $this->POST('edit_simpanan_sukarela') + $saldo_debit1;
+			}
+
+			// update buku besar
+			$this->data['entri3'] = [
+				'tgl'			=> $this->POST('edit_tgl_simpanan'),
+				'ref'			=> '103',
+				'debit'			=> $this->POST('edit_simpanan_wajib'),
+				'kredit'		=> 0,
+				'saldo_debit'	=> $saldo_debit1,
+				'saldo_kredit'	=> 0 
+			];
+			
+			$this->data['entri4'] = [
+				'tgl'			=> $this->POST('edit_tgl_simpanan'),
+				'ref'			=> '104',
+				'debit'			=> $this->POST('edit_simpanan_sukarela'),
+				'kredit'		=> 0,
+				'saldo_debit'	=> $saldo_debit2,
+				'saldo_kredit'	=> 0
+			];
+
 			$this->simpanan_m->update($this->POST('edit_id_simpanan'), $this->data['simpanan']);
+			$this->jurnal_umum_m->update_where(['id_relasi' => $this->POST('edit_id_simpanan'), 'ket' => 'Kas Simpanan Wajib'], $this->data['entri1']);
+			$this->jurnal_umum_m->update_where(['id_relasi' => $this->POST('edit_id_simpanan'), 'ket' => 'Kas Simpanan Sukarela'], $this->data['entri2']);
+			$this->buku_besar_m->update_where(['id_relasi' => $this->POST('edit_id_simpanan'), 'ket' => 'Simpanan Wajib'], $this->data['entri3']);
+			$this->buku_besar_m->update_where(['id_relasi' => $this->POST('edit_id_simpanan'), 'ket' => 'Simpanan Sukarela'], $this->data['entri4']);
+
+
 			$this->flashmsg('Data simpanan berhasil diperbarui');
 			redirect('admin/data_simpanan');
 			exit;
@@ -199,7 +254,7 @@ class Admin extends MY_Controller{
 			$anggota = $this->anggota_m->get_by_order('id_anggota', 'DESC');
 			$temp = [];
 			foreach ($anggota as $row)
-				$temp[$row->id_anggota] = $row->nama;
+				$temp[$row->id_anggota] = $row->nama;	
 			$this->data['simpanan']->dropdown = form_dropdown('edit_id_anggota', $temp, $this->data['simpanan']->id_anggota, ['class' => 'form-control']);
 			echo json_encode($this->data['simpanan']);
 			exit;
@@ -214,7 +269,7 @@ class Admin extends MY_Controller{
 		$this->data['title'] 	= "Data Simpanan | KOHIWAS";
 		$this->data['content']	= "admin/data_simpanan";
 		$this->data['anggota']	= $this->anggota_m->get_by_order('id_anggota', 'DESC');
-		$this->data['simpanan']	= $this->simpanan_m->get_by_order('id_simpanan', 'DESC');
+		$this->data['simpanan']	= $this->simpanan_m->get_by_order('id_simpanan', 'ASC');
 		$this->template($this->data);
 	}
 
@@ -222,6 +277,8 @@ class Admin extends MY_Controller{
 	{
 		$this->load->model('anggota_m');
 		$this->load->model('pinjaman_m');
+		$this->load->model('jurnal_umum_m');
+		$this->load->model('buku_besar_m');
 		
 		if ($this->POST('simpan'))
 		{
@@ -239,18 +296,19 @@ class Admin extends MY_Controller{
 				'angsuran'			=> $angsuran
 			];
 			
-			$this->load->model('jurnal_umum_m');
+			$this->pinjaman_m->insert($this->data['pinjaman']);
+
+			$this->data['getPinjaman'] = $this->pinjaman_m->get_last_row();
+			
 			$this->data['entri1'] = [
+				'id_relasi2'	=> $this->data['getPinjaman']->id_pinjaman,
 				'tgl'			=> $this->data['pinjaman']['tgl_pinjaman'],
 				'ket'			=> 'Pinjaman',
 				'debit'			=> 0,
 				'kredit'		=> $this->data['pinjaman']['jlh_pinjaman'],
 			];
 
-			$this->pinjaman_m->insert($this->data['pinjaman']);
 			$this->jurnal_umum_m->insert($this->data['entri1']);
-			
-			$this->load->model('buku_besar_m');
 
 			$cek = $this->buku_besar_m->get_last_row();
 
@@ -264,6 +322,7 @@ class Admin extends MY_Controller{
 			}
 
 			$this->data['entri2'] = [
+				'id_relasi2'	=> $this->data['getPinjaman']->id_pinjaman,
 				'tgl'			=> $this->data['pinjaman']['tgl_pinjaman'],
 				'ket'			=> 'Pinjaman',
 				'ref'			=> '105',
@@ -282,8 +341,9 @@ class Admin extends MY_Controller{
 
 		if ($this->POST('edit') && $this->POST('edit_id_pinjaman'))
 		{
+
 			$edit_bunga 	= 0.01 * $this->POST('edit_jlh_pinjaman');
-			$edit_total 	= $bunga + $this->POST('edit_jlh_pinjaman');
+			$edit_total 	= $edit_bunga + $this->POST('edit_jlh_pinjaman');
 			$edit_angsuran 	= $edit_total/$this->POST('edit_lama_pinjaman');
 
 			$this->data['pinjaman'] = [
@@ -296,7 +356,50 @@ class Admin extends MY_Controller{
 				'angsuran'			=> $edit_angsuran
 			];
 
+			// update jurnal umum pinjaman
+			$this->data['entri1'] = [
+				'tgl'			=> $this->POST('edit_tgl_pinjaman'),
+				'ket'			=> 'Pinjaman',
+				'kredit'		=> $this->POST('edit_jlh_pinjaman')
+			];
+
+			// update buku besar pinjaman
+			$cekNull = $this->buku_besar_m->get_last_row()->id_relasi2;
+			echo $cekNull; exit;
+			if($cekNull == NULL){
+				$cek = $this->buku_besar_m->get_last_row();
+				echo 'hay';
+				exit;
+			}
+			else{
+				$cek = $this->buku_besar_m->get_previous_row('id_relasi2', $this->POST('edit_id_pinjaman'), 'Pinjaman');
+				echo 'hay2';
+				exit;
+			}
+			
+			if(!isset($cek)){
+				$saldo_kredit 	= $this->POST('edit_jlh_pinjaman');
+				$saldo_debit 	= 0;
+			}
+			if(isset($cek)){
+				$prev_saldo_debit 	= $cek->saldo_debit;
+				$saldo_debit		= $prev_saldo_debit - $this->POST('edit_jlh_pinjaman');
+			}
+
+			$this->data['entri2'] = [
+				'tgl'			=> $this->POST('edit_tgl_pinjaman'),
+				'ket'			=> 'Pinjaman',
+				'ref'			=> '105',
+				'debit'			=> 0,
+				'kredit'		=> $this->POST('edit_jlh_pinjaman'),
+				'saldo_debit'	=> $saldo_debit,
+				'saldo_kredit'	=> 0 
+			];
+
 			$this->pinjaman_m->update($this->POST('edit_id_pinjaman'), $this->data['pinjaman']);
+			$this->jurnal_umum_m->update_where(['id_relasi2' => $this->POST('edit_id_pinjaman')], $this->data['entri1']);
+			$this->buku_besar_m->update_where(['id_relasi2' => $this->POST('edit_id_pinjaman')], $this->data['entri2']);
+
 			$this->flashmsg('Data pinjaman berhasil diperbarui');
 			redirect('admin/data_pinjaman');
 			exit;
@@ -331,6 +434,8 @@ class Admin extends MY_Controller{
 	{
 		$this->load->model('angsuran_m');
 		$this->load->model('pinjaman_m');
+		$this->load->model('jurnal_umum_m');
+		$this->load->model('buku_besar_m');
 		
 		if ($this->POST('simpan'))
 		{
@@ -351,18 +456,19 @@ class Admin extends MY_Controller{
 				'sisa_angsuran'		=> $sisa_angsuran
 			];
 
-			$this->load->model('jurnal_umum_m');
+			$this->angsuran_m->insert($this->data['angsuran']);
+
+			$this->data['getAngsuran']	= $this->angsuran_m->get_last_row();
+			
 			$this->data['entri1'] = [
+				'id_relasi3'	=> $this->data['getAngsuran']->id_angsuran,
 				'tgl'			=> $this->data['angsuran']['tgl_angsuran'],
 				'ket'			=> 'Angsuran',
 				'debit'			=> $this->data['angsuran']['jlh_dibayar'],
-				'kredit'		=> 0,
+				'kredit'		=> 0
 			];
 
-			$this->angsuran_m->insert($this->data['angsuran']);
 			$this->jurnal_umum_m->insert($this->data['entri1']);
-			
-			$this->load->model('buku_besar_m');
 
 			$cek = $this->buku_besar_m->get_last_row();
 
@@ -375,6 +481,7 @@ class Admin extends MY_Controller{
 			}
 
 			$this->data['entri2'] = [
+				'id_relasi3'	=> $this->data['getAngsuran']->id_angsuran,
 				'tgl'			=> $this->data['angsuran']['tgl_angsuran'],
 				'ket'			=> 'Angsuran',
 				'ref'			=> '106',
@@ -400,7 +507,39 @@ class Admin extends MY_Controller{
 				'sisa_angsuran'		=> $this->POST('edit_sisa_angsuran')
 			];
 
+			// update jurnal umum angsuran
+			$this->data['entri1'] = [
+				'tgl'			=> $this->POST('edit_tgl_angsuran'),
+				'ket'			=> 'Angsuran',
+				'debit'			=>  $this->POST('edit_jlh_dibayar'),
+				'kredit'		=> 0
+			];
+
+			// update buku besar angsuran
+			$cek = $this->buku_besar_m->get_previous_row('id_relasi3', $this->POST('edit_id_angsuran'));
+
+			if(!isset($cek)){
+				$saldo_debit 	= $this->POST('edit_jlh_dibayar');
+			}
+			if(isset($cek)){
+				$prev_saldo_debit 	= $cek->saldo_debit;
+				$saldo_debit		= $prev_saldo_debit + $this->POST('edit_jlh_dibayar');
+			}
+
+			$this->data['entri2'] = [
+				'tgl'			=> $this->POST('edit_tgl_angsuran'),
+				'ket'			=> 'Angsuran',
+				'ref'			=> '106',
+				'debit'			=> $this->POST('edit_jlh_dibayar'),
+				'kredit'		=> 0,
+				'saldo_debit'	=> $saldo_debit,
+				'saldo_kredit'	=> 0 
+			];
+
 			$this->angsuran_m->update($this->POST('edit_id_angsuran'), $this->data['angsuran']);
+			$this->jurnal_umum_m->update_where(['id_relasi3' => $this->POST('edit_id_angsuran')], $this->data['entri1']);
+			$this->buku_besar_m->update_where(['id_relasi3' => $this->POST('edit_id_angsuran')], $this->data['entri2']);
+
 			$this->flashmsg('Data angsuran berhasil diperbarui');
 			redirect('admin/data_angsuran');
 			exit;
--- application/models/Buku_besar_m.php
+++ application/models/Buku_besar_m.php
@@ -7,6 +7,18 @@ class Buku_besar_m extends MY_Model{
 		$this->data['table_name']	= 'buku_besar';
 		$this->data['primary_key']	= 'id_buku_besar';
 	}
+
+	public function get_previous_row($col, $id, $ket){
+		$query = $this->db->query('select * from '.$this->data['table_name'].' having '.$col.' <  (select '.$col.' from '.$this->data['table_name'].' where '.$col.' = '.$id.' and ket= "'.$ket.'") order by '.$col.' desc limit 1');
+
+		return $query->row();
+	}
+
+	public function get_prev_row_cond(){
+		$query = $this->db->query();
+
+		return $query->row();
+	}
 }
 
 ?>
